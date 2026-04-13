6031c8ea51a60a78e7ef5ee7a61d0be1e997580e
SSEMATA SABIRA <ssematasabira24@gmail.com>
2026-04-13 18:03:39 +0300

    Updated changes

--- config/settings.py
+++ config/settings.py
@@ -125,9 +125,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://postgres:KTXxNKUhHpRgnvdnQeLSwyUfNSMpVYED@postgres.railway.internal:5432/railway')
+_DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    'default': (
+        dj_database_url.parse(_DATABASE_URL, conn_max_age=600)
+        if _DATABASE_URL
+        else {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    )
 }
 
 
